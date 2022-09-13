1967b9b43854124d2690a4c0b24656d5f3af5da4
Nabil <holanabilkhalid@gmail.com>
2022-09-14 05:15:02 +0800

    Added remove item and search filter item application using dom/event

--- item-lister-dom/script.js
+++ item-lister-dom/script.js
@@ -1,8 +1,12 @@
 let form = document.getElementById("addForm");
 let itemsList = document.querySelector("ul");
+let searchFilter = document.getElementById("filter")
 
 form.addEventListener("submit", addItem);
+itemsList.addEventListener("click", removeItem)
+searchFilter.addEventListener("keyup", filterItems)
 
+// Function to add item
 function addItem(e){
     e.preventDefault();
 
@@ -26,6 +30,26 @@ function addItem(e){
     buttonDelete.textContent = "X";
     buttonDelete.className = "btn btn-danger btn-sm float-right delete ";
     li.appendChild(buttonDelete);
-
 }
 
+// Function to remove item
+function removeItem(e){
+    if(e.target.classList.contains("delete")){
+        if (confirm("Are you sure to delete? It can't be revert")){
+            let li = e.target.parentElement;
+            itemsList.removeChild(li);
+        }
+    }
+}
+
+// Function to filter item
+function filterItems(e){
+    // console.log(e.target.value);
+    let search = e.target.value.toLowerCase();
+    // console.log(search);
+    let li = document.querySelectorAll("li");
+    li.forEach(item =>{
+    (item.textContent.toLowerCase().indexOf(search) !== -1) 
+    ? item.style.display = "block" : item.style.display = "none";
+    })
+}
